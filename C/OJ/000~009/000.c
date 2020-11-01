d158c91abbe5a9eacf195862a20b4da303f128a2
devilYD <devilyudun@gmail.com>
2020-11-01 14:03:38 +0800

    for learn

--- C/OJ/000~009/000.c
+++ C/OJ/000~009/000.c
@@ -1,12 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-    int a,b;
+    int a, b;
 
-    scanf("%d %d",&a,&b);
+    scanf("%d %d", &a, &b);
 
-    printf("%d",a+b);
+    printf("%d", a + b);
 
     return 0;
 }
+/* 
+问题描述：计算两个整数的和。
+
+输入：输入两个整数，两个整数用空格隔开。
+
+输出：输出为两个整数的和，单独占一行。
+
+示例：
+1 1
+2
+
+*/
