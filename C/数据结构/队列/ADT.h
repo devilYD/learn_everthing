6582c2c120a43d67240d549c2427800b90f82d5d
devilYD <devilyudun@gmail.com>
2021-04-08 16:56:17 +0800

    update ADT.h

--- C/数据结构/队列/ADT.h
+++ C/数据结构/队列/ADT.h
@@ -1,50 +1,64 @@
 /* 此为队列的数组实现 */
 #include <stdlib.h>
-typedef int ElementType;//定义存储的元素类型
+typedef int ElementType; //定义存储的元素类型
 
 #ifndef _Queue_h
 
 struct QueueRecord;
 typedef struct QueueRecord *Queue;
 
-int isEmpty( Queue Q );//查询是否为空#
-int isFull( Queue Q );//查询是否为满#
-Queue CreateQueue( int MaxElements );//创建一个最多可容量MaxElements个Element的队列
-void DisposeQueue( Queue Q );//销毁队列
-void MakeEmpty( Queue Q );//清空队列
-void EnQueue( Queue Q );//入队操作
-ElementType Front( Queue Q );//返回队头元素
-void Dequeue( Queue Q );//出队操作
-ElementType FrontAndDequeue( Queue Q );//返回队头元素并出队
+int isEmpty(Queue Q);                 //查询是否为空#
+int isFull(Queue Q);                  //查询是否为满#
+Queue CreateQueue(int MaxElements);   //创建一个最多可容量MaxElements个Element的队列#
+void DisposeQueue(Queue Q);           //销毁队列#
+void MakeEmpty(Queue Q);              //清空队列
+void EnQueue(Queue Q);                //入队操作
+ElementType Front(Queue Q);           //返回队头元素
+void Dequeue(Queue Q);                //出队操作
+ElementType FrontAndDequeue(Queue Q); //返回队头元素并出队
 
 #endif /* _Queue_h */
 
 struct QueueRecord
 {
-    int Capacity;
+    int Capacity; //容量
     int Front;
     int Rear;
     int Size;
     ElementType *Array;
 };
 
-int isEmpty( Queue Q )
+int isEmpty(Queue Q)
 {
     return Q->Size == 0;
 }
 
-int isFull( Queue Q )
+int isFull(Queue Q)
 {
-    int x = sizeof( Q->Array ) / sizeof( ElementType );
-    return Q->Size == x;
+    return Q->Size == Q->Capacity;
 }
 
-Queue CreateQueue( int MaxElements )
+Queue CreateQueue(int MaxElements)
 {
-    Queue Q = ( struct QueueRecord * ) malloc( sizeof ( struct QueueRecord ) );
-    Q->Array = ( ElementType * ) malloc( sizeof(ElementType) * MaxElements );
+    Queue Q = (struct QueueRecord *)malloc(sizeof(struct QueueRecord));
+    Q->Array = (ElementType *)malloc(sizeof(ElementType) * MaxElements);
+    Q->Capacity = MaxElements;
     Q->Front = 1;
     Q->Size = 0;
     Q->Rear = 0;
     return Q;
 }
+
+void DisposeQueue(Queue Q)
+{
+    free(Q->Array);
+    free(Q);
+}
+
+void MakeEmpty(Queue Q)
+{
+    Q->Size = 0;
+    Q->Front = 1;
+    Q->Rear = 0;
+}
+
