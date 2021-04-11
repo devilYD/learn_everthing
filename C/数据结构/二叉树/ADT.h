44f14d2ead7c0a29cb95423c1f0c4afef3a99b28
devilYD <devilyudun@gmail.com>
2021-04-11 14:03:24 +0800

    update ADT.h

---
+++ C/数据结构/二叉树/ADT.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+typedef int ElementType; //表中所储存的元素类型
+
+#ifndef _Tree_H
+
+struct TreeNode;
+typedef struct TreeNode *Position;
+typedef struct TreeNode *SearchTree;
+
+SearchTree MakeEmpty( SearchTree T );//清空树
+Position Find( ElementType X, SearchTree T);//查找
+Position FindMin( SearchTree T);//查找最小
+Position FindMax( SearchTree T);//查找最大
+SearchTree Insert( ElementType X,SearchTree T);//插入
+SearchTree Delete( ElementType X,SearchTree T);//删除
+ElementType Retrieve(Position P);//检索
+
+#endif /* _Tree_H */
