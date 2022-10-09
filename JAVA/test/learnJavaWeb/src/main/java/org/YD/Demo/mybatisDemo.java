065f24f210254c11ef3bfc1278dafa2f71ca9ee6
devilYD <devilyudun@gmail.com>
2022-10-09 18:07:35 +0800

    测试MyBatis导入成功

---
+++ JAVA/test/learnJavaWeb/src/main/java/org/YD/Demo/mybatisDemo.java
@@ -0,0 +1,25 @@
+package org.YD.Demo;
+
+import org.YD.Pojo.roomMate;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class mybatisDemo {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        List<roomMate> roomMates = sqlSession.selectList("org.YD.Pojo.selectRoomMate");
+
+        System.out.println(roomMates);
+    }
+}
