fbad6f5f2c2f71cef7f318c9a165a0d1c300ce29
devilYD <devilyudun@gmail.com>
2022-10-09 21:55:33 +0800

    update

--- JAVA/test/learnJavaWeb/src/main/java/org/YD/mapper/RoomMapper.java
+++ JAVA/test/learnJavaWeb/src/main/java/org/YD/mapper/RoomMapper.java
@@ -1,9 +1,9 @@
 package org.YD.mapper;
 
 import org.YD.Pojo.roomMate;
-
 import java.util.List;
 
 public interface RoomMapper {
-    List<roomMate> selectRoomMate();
+    public List<roomMate> selectRoomMate();
+    public List<roomMate> selectByName();
 }
