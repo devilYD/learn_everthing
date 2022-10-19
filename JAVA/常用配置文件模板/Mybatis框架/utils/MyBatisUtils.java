9339bb1df53312de510d25c6a6f9cc7796081c1d
devilYD <devilyudun@gmail.com>
2022-10-19 15:12:23 +0800

    update

--- JAVA/常用配置文件模板/Mybatis框架/utils/MyBatisUtils.java
+++ JAVA/常用配置文件模板/Mybatis框架/utils/MyBatisUtils.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 
+// SqlSession sqlsession = MyBatisUtils.getSqlSession();
+// sqlsession.close();
+
 public class MyBatisUtils {
     private static SqlSessionFactory factory=null;
     static {
