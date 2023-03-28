98ca17e6e31c77c53ee97d60070237d27d503472
devilYD <devilyudun@gmail.com>
2023-03-28 17:52:37 +0800

    update

---
+++ JAVA/test/LearnSomething/src/main/java/org/yd/LearnSomethingApplication.java
@@ -0,0 +1,13 @@
+package org.yd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSomethingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSomethingApplication.class, args);
+    }
+
+}
---
+++ JAVA/test/LearnSomething/src/test/java/org/yd/LearnSomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSomethingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
