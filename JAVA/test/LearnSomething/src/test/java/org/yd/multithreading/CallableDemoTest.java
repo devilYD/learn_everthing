d02308c9bbe432a013b868304f5916f88c7e2ef1
devilYD <devilyudun@gmail.com>
2023-03-30 17:30:21 +0800

    使用Callable进行多线程开发

--- JAVA/test/LearnSomething/src/test/java/org/yd/multithreading/CallableDemoTest.java
+++ JAVA/test/LearnSomething/src/test/java/org/yd/multithreading/CallableDemoTest.java
@@ -8,18 +8,22 @@ import java.util.concurrent.FutureTask;
 public class CallableDemoTest {
     @Test
     public void callTest() throws ExecutionException, InterruptedException {
+        // Callable实体类
         CallableDemo callableDemo0 = new CallableDemo("A");
         CallableDemo callableDemo1 = new CallableDemo("B");
         CallableDemo callableDemo2 = new CallableDemo("C");
 
+        // 初始化FutureTask对象用于接收返回值
         FutureTask<String> futureTask0 = new FutureTask<>(callableDemo0);
         FutureTask<String> futureTask1 = new FutureTask<>(callableDemo1);
         FutureTask<String> futureTask2 = new FutureTask<>(callableDemo2);
 
+        // 使用Thread的start方法运行多线程
         new Thread(futureTask0).start();
         new Thread(futureTask1).start();
         new Thread(futureTask2).start();
 
+        // 使用FutureTask对象的get方法获取返回值
         System.out.println(futureTask0.get());
         System.out.println(futureTask1.get());
         System.out.println(futureTask2.get());
